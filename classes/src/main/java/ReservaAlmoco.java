0037964e2b9e2523d88cb1bf6281a62bfdb1082d
Letícia Moreira <leticia.m@gec.inatel.br>
2022-05-18 21:09:26 -0300

    codigo

--- classes/src/main/java/ReservaAlmoco.java
+++ classes/src/main/java/ReservaAlmoco.java
@@ -2,12 +2,22 @@ public class ReservaAlmoco extends Reserva{
 
     private float precoAlmoco;
 
-    public ReservaAlmoco(int qtdPessoas) {
-        super(qtdPessoas);
-    }
     @Override
     public void mostraInfo(){
         super.mostraInfo();
         System.out.println("Preço por pessoa: " + this.precoAlmoco);
+    }
+
+    public ReservaAlmoco(int qtdPessoas, float precoAlmoco) {
+        super(qtdPessoas);
+        this.precoAlmoco = precoAlmoco;
+    }
+
+    public float getPrecoAlmoco() {
+        return precoAlmoco;
+    }
+
+    public void setPrecoAlmoco(float precoAlmoco) {
+        this.precoAlmoco = precoAlmoco;
     }
 }
