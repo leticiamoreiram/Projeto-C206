1ce716c7b29ded422c6b8f135c07790d5062fe61
Letícia Moreira <leticia.m@gec.inatel.br>
2022-05-20 15:30:15 -0300

    codigo

--- classes/src/main/java/classes/Garcom.java
+++ classes/src/main/java/classes/Garcom.java
@@ -6,5 +6,25 @@ public class Garcom {
     private String turno;
     private int id;
 
+    public Garcom(String nome, String turno, int id) {
+        this.nome = nome;
+        this.turno = turno;
+        this.id = id;
+    }
 
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
