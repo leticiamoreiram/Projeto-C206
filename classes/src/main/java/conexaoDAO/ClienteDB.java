faffa5a0f62c71b05a99011fa5256c4c465bad34
Letícia Moreira <leticia.m@gec.inatel.br>
2022-05-31 14:12:21 -0300

    conexão mySQL

---
+++ classes/src/main/java/conexaoDAO/ClienteDB.java
@@ -0,0 +1,128 @@
+package conexaoDAO;
+
+import classes.Cliente;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ClienteDB extends Database {
+
+    // ----------------------------INSERINDO NOVO REGISTRO----------------------------
+    public boolean inserirCliente(Cliente cliente){
+        connect();
+        String sql = "INSERT INTO Cliente(nome, email, telefone, cpf ) VALUES(?, ?, ?, ?)";
+        try {
+
+            pst = connection.prepareStatement(sql);
+            pst.setString(1, cliente.getNome());      // concatena nome na primeira ? do comando
+            pst.setString(2, cliente.getEmail());        // concatena nome na segunda ? do comando
+            //pst.setString(3, cliente.getTelefone());        // concatena nome na terceira ? do comando
+            pst.setString(4, cliente.getCpf());        // concatena nome na quarta ? do comando
+            pst.execute();                           // executa o comando
+            check = true;
+
+        } catch (SQLException e) {
+            System.out.println("Erro de operação: " + e.getMessage());
+            check = false;
+        }
+        finally {
+            try{
+                connection.close();
+                pst.close();
+            }catch (SQLException e){
+                System.out.println("Erro ao fechar a conexão: " + e.getMessage());
+            }
+        }
+        return check;
+    }
+
+    // ----------------------------BUSCANDO TODOS REGISTROS----------------------------
+    public ArrayList<Cliente> buscarCliente(){
+        connect();
+        ArrayList<Cliente> listaClientes = new ArrayList<>();
+        String sql = "SELECT * FROM Cliente";
+        try{
+            statement = connection.createStatement();
+            result = statement.executeQuery(sql);
+
+            while(result.next()){
+                Cliente clienteTemp = new Cliente();
+                clienteTemp.setCpf(result.getString("cpf"));
+                //clienteTemp.setTelefone(result.getString("telefone"));
+                clienteTemp.setEmail(result.getString("email"));
+                clienteTemp.setNome(result.getString("nome"));
+
+                System.out.println("ID = " + clienteTemp.getNome());
+                System.out.println("Nome = " + clienteTemp.getEmail());
+                // System.out.println("CPF = " + clienteTemp.getTelefone());
+                System.out.println("CPF = " + clienteTemp.getCpf());
+                System.out.println("------------------------------");
+
+                listaClientes.add(clienteTemp);
+            }
+            check = true;
+        }catch (SQLException e){
+            System.out.println("Erro de operação: " + e.getMessage());
+            check = false;
+        }finally {
+            try {
+                connection.close();
+                statement.close();
+                result.close();
+            }catch (SQLException e){
+                System.out.println("Erro ao fechar a conexão: " + e.getMessage());
+            }
+        }
+        return listaClientes;
+    }
+
+    // ----------------------------ATUALIZANDO NOME NO REGISTRO----------------------------
+    public boolean atualizarCliente(String cpf, Cliente cliente){
+        connect();
+        String sql = "UPDATE Cliente SET nome=?, email=?, telefone=? WHERE cpf=?";
+        try{
+            pst = connection.prepareStatement(sql);
+            pst.setString(1, cliente.getNome());
+            pst.setString(2, cliente.getEmail());
+            //  pst.setString(3, cliente.getTelefone());
+            pst.setString(4, cpf);
+            pst.execute();
+            check = true;
+        }catch (SQLException e){
+            System.out.println("Erro de operação: " + e.getMessage());
+            check = false;
+        }finally {
+            try {
+                connection.close();
+                pst.close();
+            }catch (SQLException e) {
+                System.out.println("Erro ao fechar a conexão: " + e.getMessage());
+            }
+        }
+        return check;
+    }
+
+    // ----------------------------EXCLUINDO REGISTRO----------------------------
+    public boolean deletarCliente(String cpf) {
+        connect();
+        String sql = "DELETE FROM Cliente where cpf=?";
+        try{
+            pst = connection.prepareStatement(sql);
+            pst.setString(1, cpf);
+            pst.execute();
+            check = true;
+        }catch (SQLException e){
+            System.out.println("Erro de operação: " + e.getMessage());
+            check = false;
+        }finally {
+            try {
+                connection.close();
+                pst.close();
+            }catch (SQLException e){
+                System.out.println("Erro ao fechar a conexão: " + e.getMessage());
+            }
+        }
+        return check;
+    }
+
+}
--- classes/src/main/java/conexaoDAO/Principal.java
+++ classes/src/main/java/conexaoDAO/Principal.java
@@ -3,8 +3,8 @@ package conexaoDAO;
 public class Principal {
 
     public static void main(String[] args) {
-        Database database = new Database();
-        database.connect();
+        //Database database = new Database();
+        //database.connect();
 
     }
 }
